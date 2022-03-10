ab3b85b960e3d51e0d2fafd6a57e2ff8c81fe1e2
Danny Santino <dannysantisimo@gmail.com>
2022-03-10 02:38:06 +0100

    redefine admin routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,30 +6,20 @@ const { verifyTokenAndAuth, verifyTokenAndAdmin } = require('../utilities/verify
 
 const router = require('express').Router();
 
-router.put('/:id', verifyTokenAndAuth, catchAsync(async (req, res) => {
-    const { password: userPassword } = req.body;
-    userPassword && (req.body.password = CryptoJS.AES.encrypt(userPassword, process.env.CRYPTO_JS_SECRET).toString());
-    const updatedUser = await User.findByIdAndUpdate(req.user.id, {
-        $set: req.body
-    }, { new: true });
-    const { password, ...rest } = updatedUser._doc;
-    res.status(200).json(rest);
-}));
-
 router.get('/admin/:id', verifyTokenAndAdmin, catchAsync(async (req, res) => {
     const user = await User.findById(req.params.id);
     const { password, ...rest } = user._doc;
     res.status(200).json(rest);
 }));
 
-router.get('/', verifyTokenAndAdmin, catchAsync(async (req, res) => {
+router.get('/admin/useraccounts', verifyTokenAndAdmin, catchAsync(async (req, res) => {
     const users = req.query.new
         ? await User.find().sort({ _id: -1 }).limit(1)
         : await User.find();
     res.status(200).json(users);
 }));
 
-router.get('/stats', verifyTokenAndAdmin, catchAsync(async (req, res) => {
+router.get('/admin/stats', verifyTokenAndAdmin, catchAsync(async (req, res) => {
     const date = new Date();
     const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
     const data = await User.aggregate([
@@ -51,6 +41,16 @@ router.get('/stats', verifyTokenAndAdmin, catchAsync(async (req, res) => {
         }
     ]);
     res.status(200).json(data);
+}));
+
+router.put('/update/:id', verifyTokenAndAuth, catchAsync(async (req, res) => {
+    const { password: userPassword } = req.body;
+    userPassword && (req.body.password = CryptoJS.AES.encrypt(userPassword, process.env.CRYPTO_JS_SECRET).toString());
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, {
+        $set: req.body
+    }, { new: true });
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
 }));
 
 router.delete('/:id', verifyTokenAndAuth, catchAsync(async (req, res) => {
