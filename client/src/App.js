61ca69c5aa3f7d5610b2e23d0d7f7322e3870311
Danny Santino <dannysantisimo@gmail.com>
2022-03-21 18:55:15 +0100

    install react-router-dom and setup client routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import Product from './pages/Product';
+import Cart from './pages/Cart';
 import About from './pages/About';
 import Blog from './pages/Blog';
-import Cart from './pages/Cart';
 import Contact from './pages/Contact';
 import Newsletter from './components/Newsletter';
 import Footer from './components/Footer';
@@ -13,12 +15,20 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Contact />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='shop' element={<Shop />} />
+        <Route exact path='product' element={<Product />} />
+        <Route exact path='cart' element={<Cart />} />
+        <Route exact path='contact' element={<Contact />} />
+        <Route exact path='about' element={<About />} />
+        <Route exact path='blog' element={<Blog />} />
+      </Routes>
       <Newsletter />
       <Footer />
-    </>
+    </Router>
   );
 }
 
