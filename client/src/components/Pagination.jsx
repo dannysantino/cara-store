2eb7cbf224368f3fee175e25fdf150b20191beb6
Danny Santino <dannysantisimo@gmail.com>
2022-03-27 12:54:58 +0100

    remove excessive padding on top and bottom

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,19 +1,21 @@
+import { Link } from 'react-router-dom'
+
 import '../stylesheets/Pagination.css'
 
 const Pagination = () => {
     return (
-        <div id='pagination' className='py-5'>
+        <div id='pagination'>
             <nav aria-label='...'>
                 <ul className='pagination'>
                     <li className='page-item disabled'>
-                        <a className='page-link'>Previous</a>
+                        <Link className='page-link'>Previous</Link>
                     </li>
                     <li className='page-item active' aria-current='page'>
-                        <a className='page-link' href='#'>1</a>
+                        <Link className='page-link' to='#'>1</Link>
                     </li>
-                    <li className='page-item'><a className='page-link tag' href='#'>2</a></li>
-                    <li className='page-item'><a className='page-link tag' href='#'>3</a></li>
-                    <li className='page-item'><a className='page-link tag' href='#'>Next</a></li>
+                    <li className='page-item'><Link className='page-link tag' to='#'>2</Link></li>
+                    <li className='page-item'><Link className='page-link tag' to='#'>3</Link></li>
+                    <li className='page-item'><Link className='page-link tag' to='#'>Next</Link></li>
                 </ul>
             </nav>
         </div>
