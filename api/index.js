2d17076e365b5f640fef1bbf64136d6b8a4d2a4a
Danny Santino <dannysantisimo@gmail.com>
2022-04-01 13:19:10 +0100

    fix cors policy issue

--- api/index.js
+++ api/index.js
@@ -19,6 +19,11 @@ mongoose.connect(process.env.MONGO_URI, {
     .catch(e => console.error('Error connecting to database', e));
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
