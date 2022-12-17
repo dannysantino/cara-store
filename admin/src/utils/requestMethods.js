e6d3b20078bdc447b3061e449b59d5f01e940ef4
Danny Santino <dev@dannysantino.com>
2022-12-17 21:43:42 +0100

    chore: replace hardcoded api `BASE_URL` value with env variable

--- admin/src/utils/requestMethods.js
+++ admin/src/utils/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import jwt_decode from 'jwt-decode'
 
-const BASE_URL = 'https://carastore.onrender.com/api';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
